65692d1523ddc47491c97c8897f4c72e8643c29e
chris-wamae <chriswamae123@gmail.com>
2024-10-18 08:52:43 +0800

    Updated appSettings.json

--- back-end/DietSaint/obj/Debug/net8.0/DietSaint.AssemblyInfo.cs
+++ back-end/DietSaint/obj/Debug/net8.0/DietSaint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DietSaint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c645c31bdf91ee1d57114c220a73ca1a1cb9dc60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee97de9b9f5277578a2fc9248f85bee946838d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DietSaint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DietSaint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
